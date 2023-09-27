1c36ab8fc560a3c9ea609951ba559dee41e6796b
Jer-Son <jersonhenao30@gmail.com>
2023-09-27 11:05:10 -0500

    FechasLibreria

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,7 @@
+const diaHoy = new Date();
+let valor= diaHoy;
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getTime();
+console.log(valor);
